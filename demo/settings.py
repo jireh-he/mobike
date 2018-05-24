638a42d2e7408219a01f312144b49ee1697b306f
jireh <18772119@qq.com>
2018-05-24 17:33:19 +0800

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ato',
         'USER': 'root',
-        'PASSWORD': 'Xm123456',
+        'PASSWORD': '*****',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'CHARSET': 'utf8',
